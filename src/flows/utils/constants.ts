dab967674d80de359ec82bd2c3073bbf1a924e5d
rodric rabbah <rodric.rabbah@postman.com>
2025-05-14 12:38:22 -0400

    Update constants.ts

--- src/flows/utils/constants.ts
+++ src/flows/utils/constants.ts
@@ -1,6 +1,6 @@
 import { CallToolResult } from '@modelcontextprotocol/sdk/types.js'
 
-export const FLOWS_API_BASE = 'https://postman.flows.pstmn-beta.io/api'
+export const FLOWS_API_BASE = 'https://macho-dropkick-stun.flows.pstmn.io/api'
 
 export function toolCallError(msg?: string): CallToolResult {
     return {
